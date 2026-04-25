9029831a7d77c69e3f44d67c8cbfce7afd5611dd
Patriot Benton <patriotbenton04@gmail.com>
2026-04-25 19:41:02 -0400

    Initial framework of top level file

---
+++ AMBA_AXI_Stream.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+--Valid - Ready Handshake 
+entity AMBA_AXI_Stream is 
+    port(
+
+    )
+end entity;
+
+architecture fpga of AMBA_AXI_Stream is
+
+
+begin
+
+
+end architecture;
